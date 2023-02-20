c2b2185f51346decd17c29f9e3d610532403bbd8
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2023-02-20 17:42:37 +0400

    Fix permissions

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd webrtc_android
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PATH:$PWD/depot_tools
 
-sudo apt install python-is-python3
+apt install python-is-python3
 
 fetch --nohooks webrtc_android
 gclient sync
